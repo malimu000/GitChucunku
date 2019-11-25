6992b821c522db8076bfa510eed4b175e18497fe
DELL <DELL@DESKTOP-BOJP1OH>
2019-11-25 16:08:07 +0800

    卡阿卡

--- src/cn/Caca.java
+++ src/cn/Caca.java
@@ -5,7 +5,7 @@ public class Caca {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("啊发我发我福娃发我");
+		System.out.println("鍝堝搱鍝堝搱鍝堝搱");
 	}
 
 }
